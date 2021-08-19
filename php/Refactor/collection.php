42cd589e04eb204dae62ce9f92bd7270d6c2f62b
Lee Weiquan <weiquan@togalimited.com>
2021-08-19 22:06:12 +0800

    Interface ArrayAccess, Countable, IteratorAggregate

--- php/Refactor/collection.php
+++ php/Refactor/collection.php
@@ -1,6 +1,28 @@
 <?php
 
-class Collection
+// ON https://www.php.net/manual/en/class.arrayaccess.php
+// interface ArrayAccess
+// {
+//     // Allow the collection to respond to `isset($items['key'])` checks
+//     abstract public function offsetExists($offset);
+
+//     // Allow retrieving an item from the collection using `$items['key']`
+//     abstract public function offsetGet($offset);
+
+//     // Allow adding an item to the end of the collection using `$items[] =
+//     // $foo` as well as at a specific key using `$items['key'] = $foo`
+//     abstract public function offsetSet($offset, $value);
+
+//     // Allow removing an item from the collection using `unset($items['key'])
+//     abstract public function offsetUnset($offset);
+// }
+
+// interface Countable
+// {
+//     // Allow the collection to respond to `isset($items['key'])` checks
+//     abstract public function count();
+// }
+class Collection implements ArrayAccess, Countable, IteratorAggregate
 {
     protected $items;
 
@@ -12,6 +34,40 @@ class Collection
     public static function make($items)
     {
         return new static ($items);
+    }
+
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->items);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->items[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if ($offset == null) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
+    }
+
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
     }
 
     public function map($callback)
@@ -30,4 +86,17 @@ class Collection
     }
 }
 
-var_dump(new Collection([1,2,3,4,5]));
+
+$employees = new Collection([
+    ['name' => 'Mary', 'email' => 'mary@example.com', 'salaried' => true],
+    ['name' => 'John', 'email' => 'john@example.com', 'salaried' => false],
+    ['name' => 'Kelly', 'email' => 'kelly@example.com', 'salaried' => true],
+]);
+
+// var_dump($employees->filter(function ($employee) {
+//     return $employee['salaried'];
+// })->count());
+
+foreach ($employees as $item) {
+    var_dump($item);
+}
