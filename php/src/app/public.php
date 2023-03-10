a2629b2c6ef10705349530a72314f8884083c177
Lee Weiquan <weiquan@togalimited.com>
2023-03-10 13:58:56 +0800

    chore: custom autoloading

--- php/src/app/public.php
+++ php/src/app/public.php
@@ -1,38 +1,15 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
-// require 'php/vendor/autoload.php';
-require_once './PaymentGateway/Stripe/Transaction.php';
-require_once './PaymentGateway/Paddle/Transaction.php';
-require_once './PaymentGateway/Paddle/CustomerProfile.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-use PaymentGateway\Paddle\Transaction as PaddleTransaction;
-use PaymentGateway\Stripe\Transaction;
+use App\PaymentGateway\Stripe\Transaction;
+use App\PaymentGateway\Paddle\Transaction as PaddleTransaction;
 
 $transactionStripe = new Transaction();
 $transactionPaddle = new PaddleTransaction();
 // $transactionPaddle = new PaymentGateway\Paddle\Transaction();
 
-var_dump($transactionStripe);
-var_dump($transactionPaddle);
-// var_dump($transactionPaddle);
-// $root = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
-
-// define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
-// define('FILES_PATH', $root . 'transaction_files'. DIRECTORY_SEPARATOR);
-// define('VIEWS_PATH', $root . 'views'. DIRECTORY_SEPARATOR);
-
-
-// require APP_PATH . "App.php";
-
-// $files = getTransactionFiles(FILES_PATH);
-
-// $transactions = [];
-
-// foreach($files as $file) {
-//     $transactions = array_merge($transactions, getTransactions($file));
-// }
-
-// require VIEWS_PATH . 'transaction.php';
-// dd($transactions[1]);
+dump($transactionStripe);
+dd($transactionPaddle);
