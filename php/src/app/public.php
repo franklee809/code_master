ce3061d451866f15829ca4ccfe1513956798b351
Lee Weiquan <weiquan@togalimited.com>
2023-04-07 13:14:34 +0800

    chore: php tutorial
    - late static binding

--- php/src/app/public.php
+++ php/src/app/public.php
@@ -2,9 +2,13 @@
 
 declare(strict_types = 1);
 
-use App\Invoice;
+use App\ClassA;
+use App\ClassB;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-$invoice = new Invoice();
-var_dump($invoice);
+$classA        = ClassA::make();
+$classB        = ClassB::make();
+
+dump($classA);
+dd($classB);
