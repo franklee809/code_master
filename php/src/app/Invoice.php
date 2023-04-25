06ad97b96471cfa98b362778d1502dccdb4b98af
Lee Weiquan <weiquan@togalimited.com>
2023-04-25 13:19:35 +0800

    chore: php tutorial serialization - wakeup sleep serialize and unserialize magic methods

--- php/src/app/Invoice.php
+++ php/src/app/Invoice.php
@@ -4,11 +4,41 @@ namespace App;
 
 class Invoice
 {
+    private string $id;
 
-    public function __construct(public Customer $customer, public float $amount, private int $id) {
-    }
-    public function __debugInfo()
+    public function __construct(public float $amount, public string $description, public string $creditCardNo)
     {
-        return [1];
+        $this->id = uniqid('invoice_');
+    }
+
+    public static function create(): static
+    {
+        return new static(123.0, "test", "1423 2323 2323 2323");
+    }
+
+    // used before the serialization happens
+    public function __sleep(): array
+    {
+        return ['id', 'amount'];
+    }
+
+    public function __wakeup()
+    {
+        // return [];
+    }
+
+    public function __serialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'description' => $this->description,
+            'creditCardNo' => base64_encode($this->creditCardNo),
+        ];
+    }
+
+    public function __unserialize($data): void
+    {
+        dd($data);
     }
 }
---
+++ php/src/app/serialize.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Invoice;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+// dump(serialize(true));
+// dump(serialize(1));
+// dump(serialize(2.5));
+// dump(serialize('test'));
+// dump(serialize([1,2,3]));
+// dd(serialize([1 => '123',2 => '12323',3 => '1231231']));
+
+$invoice       = new Invoice(123.0, "test", "1423 2323 2323 2323");
+$string        = serialize($invoice);
+
+$class         = unserialize($string);
+
+dd($invoice, $class, $string === $class);
