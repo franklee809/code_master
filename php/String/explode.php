b54463292cdcb132ed9f85d86e8f94ef1424fff3
Lee Weiquan <weiquan@togalimited.com>
2023-06-16 15:23:47 +0800

    chore: update

--- php/String/explode.php
+++ php/String/explode.php
@@ -1,8 +1,5 @@
-<?php 
+<?php
 
     $fruits = "apple,pear,grapefruit,cherry";
     // print_r(explode(',', $fruits,10));
     print_r(explode(',', $fruits,-2));
-
-
-?>
---
+++ python/lab.py
@@ -0,0 +1,6 @@
+
+array = list()
+for i in range(0, 3):
+    array.append(int(input('Enter a number:')))
+array.sort()
+print(array)
